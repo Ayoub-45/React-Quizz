535ee8614d86c08f9dae4591b01a79ae3573b036
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-04 19:09:54 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import DateCounter from "./DateCounter";
+
+export default function App(){
+    return (
+        <div>
+            <Header/>
+            <DateCounter/>
+            
+        </div>
+    )
+}
