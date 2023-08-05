fccd5e1cb53a207075fec1336faab377ad554d99
unknown <ayoubnasraoui161@yahoo.fr>
2023-08-05 19:40:54 +0200

    Create a fake API then use useEffect to fetch data finally perform dispatch using useReducer hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import Header from "./Header";
-
-export default function App(){
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+const initalState = {
+    questions: [],
+    //Loading,"error","ready","active","finished"
+    status:"Loading"
+};
+function reducer(state, action) {
+    switch(action.type){
+        case "dataReceived":
+            return {...state,questions:action.payload,status:"ready"}
+        
+        case "dataFailed":
+            return {...state,status:"error"};
+        default:
+            throw new Error("Action is unkown")
+    }
+}
+export default function App() {
+    const [state, dispatch] = useReducer(reducer, initalState);
+    useEffect(function () {
+        fetch("http://localhost:8000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({type:"dataReceived",payload:data}))
+            .catch((err) => dispatch({type:"dataFailed"}));
+    }, []);
     return (
         <div className="app">
-            <Header/>
-            
+            <Header />
+            <Main>
+                <p>1/15</p>
+                <p>Question</p>
+            </Main>
         </div>
-    )
+    );
 }
